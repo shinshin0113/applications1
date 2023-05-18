b0caf26b50731dff68066dd81e1a8c0dd03c4508
shinshin0113 <130219147+shinshin0113@users.noreply.github.com>
2023-05-18 14:22:23 +0000

    [Finish]3-11-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,13 +2,13 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
-  
+
   def create
     list = List.new(list_params)
     list.save
     redirect_to list_path(list.id)
   end
-  
+
   def index
     @lists = List.all
   end
@@ -20,30 +20,30 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
-  def update 
+
+  def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])
     list.destroy
     redirect_to '/lists'
   end
-  
+
   def create
-    @list = List.new(list_params)
-    if @list.save
-      redirect_to list_path(@list.id)
-    else
-      render :new
-    end
+
+    list = List.new(list_params)
+    list.save
+    flash[:notice] = "投稿が成功しました"
+    redirect_to list_path(list.id)
+
   end
-  
+
   private
-  
+
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
